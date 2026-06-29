a9501ecfb1e9b967f05ce962d7ba2aec962fc984
Jack Tonkin <jack@tonkin.org.uk>
2026-06-29 13:58:39 +0100

    Export libgit2@1.5 as keg-only; for building splitsh/lite

---
+++ Formula/libgit2@1.5.rb
@@ -0,0 +1,60 @@
+class Libgit2AT15 < Formula
+  desc "C library of Git core methods that is re-entrant and linkable"
+  homepage "https://libgit2.github.com/"
+  url "https://github.com/libgit2/libgit2/archive/v1.5.2.tar.gz"
+  sha256 "57638ac0e319078f56a7e17570be754515e5b1276d3750904b4214c92e8fa196"
+  license "GPL-2.0-only" => { with: "GCC-exception-2.0" }
+  head "https://github.com/libgit2/libgit2.git", branch: "main"
+
+  keg_only :versioned_formula
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libssh2"
+
+  def install
+    args = std_cmake_args
+    args << "-DBUILD_EXAMPLES=YES"
+    args << "-DBUILD_TESTS=OFF" # Don't build tests.
+    args << "-DUSE_SSH=YES"
+
+    mkdir "build" do
+      system "cmake", "..", *args
+      system "make", "install"
+      cd "examples" do
+        (pkgshare/"examples").install "lg2"
+      end
+      system "make", "clean"
+      system "cmake", "..", "-DBUILD_SHARED_LIBS=OFF", *args
+      system "make"
+      lib.install "libgit2.a"
+    end
+  end
+
+  test do
+    (testpath/"test.c").write <<~EOS
+      #include <git2.h>
+      #include <assert.h>
+
+      int main(int argc, char *argv[]) {
+        int options = git_libgit2_features();
+        assert(options & GIT_FEATURE_SSH);
+        return 0;
+      }
+    EOS
+    libssh2 = Formula["libssh2"]
+    flags = %W[
+      -I#{include}
+      -I#{libssh2.opt_include}
+      -L#{lib}
+      -lgit2
+    ]
+    system ENV.cc, "test.c", "-o", "test", *flags
+    system "./test"
+  end
+end
